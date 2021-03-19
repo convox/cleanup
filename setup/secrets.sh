8e5ef5b9bd89b91c85d5d2d3ec598bf4af12ffc2
David Dollar <david@dollar.io>
2021-03-19 12:01:30 -0400

    get rid of set-env

--- setup/secrets.sh
+++ setup/secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export_secret() {
-  echo "${1}=$(echo $SECRETS | jq -r ".${1}")" >> $GITHUB_ENV
+	echo "${2:-$1}=$(echo $SECRETS | jq -r ".${1}")" >> $GITHUB_ENV
 }
 
 case "$PROVIDER" in
